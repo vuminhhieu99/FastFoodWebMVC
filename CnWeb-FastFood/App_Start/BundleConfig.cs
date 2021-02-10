fac806e4e455939ce1075cd489e9faa7785e902f
vuhieupro1999@gmail.com <vuhieupro1999@gmail.com>
2021-02-10 22:12:47 +0700

    Add detail interfaces for admin

--- CnWeb-FastFood/App_Start/BundleConfig.cs
+++ CnWeb-FastFood/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace CnWeb_FastFood
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -25,6 +26,12 @@ namespace CnWeb_FastFood
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/bundles/css/base").Include(
+                 "~/Assets/Client/css/elegant-icons.css",
+                "~/Assets/Client/css/nice-select.css",               
+                "~/Assets/Client/css/style.css"
+                ));
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
